e84f872aa563a4c07ea4be30023c054ed09306b8
Margo <leonetti.work@gmail.com>
2025-10-23 01:11:21 +0200

    Add source files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
-import BugTracker from './components/BugTracker'
-
-export default function App() {
-  return <BugTracker />
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-4xl font-bold text-center">Bug Tracker Pro</h1>
+    </div>
+  )
 }
+
+export default App
